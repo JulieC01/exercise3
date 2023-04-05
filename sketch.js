d11f9970c9a90a459a3eec02bf3a8fb33264599d
JulieC01 <124850153+JulieC01@users.noreply.github.com>
2023-04-05 14:19:41 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+let input, button, result, question, submit, answer;
+//global is variable on top that makes it available 
+//throughout rather than in functions, so don't need to say let. 
+//So can use variable result again elsewhere than in just a function.
+let Answer1;
+//let c;// lets have to be on top
+let colorInput = document.getElementById('color-input');
+
+function setup() {
+    // make the div called "mySketch" a p5.js canvas element
+    let cnv = createCanvas(windowWidth, windowHeight);
+    cnv.parent('mySketch');
+
+    result = createElement('p', 'What food would you like to eat at Japantown right now?');
+    result.style('font-size', '30px');
+    result.style('margin', '0px');
+    result.style('text-align', 'center');
+    result.id('answer');
+    answer = document.getElementById('answer');  //answer variable becomes answer, document is a key part of js, term to document this is what that elemant is.
+    //allows you to place this into any html file
+    colorInput.appendChild(answer);  //grabbing the element that have variable answer. Above says theres element of color. This one create a container for the elements within color
+//append means placing child to be with the parent container/div
+    //answer and colorinput line is saying the paragraph is that.
+
+
+    input = createInput(''); //creating a input element. Input is where u type in the box, giving it the ID question below
+    input.id('question');
+
+    question = document.getElementById('question'); //giving id by question
+    colorInput.appendChild(question); //putting it into colorinput again
+
+  
+    button = createButton('submit');
+    button.id('submit'); //has id of submit
+    submit = document.getElementById('submit'); 
+    colorInput.appendChild(submit); //putting into colorinput
+    button.mousePressed(output);
+
+  }
+  
+  function draw() {
+
+  }
+  
+  function windowResized() {
+    // resize the canvas when the screen is resized.
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function myInputEvent() {
+  console.log('you are typing: ', this.value());
+}
+
+function output() { 
+  const answers = input.value(); //const is another initializing word like Let. Const says is staying thr same, let changes.
+  input.value(''); 
+
+  let answer1 = createP(''); //input value is empty, need it to read value and find the one ur looking for
+  answer1.id('answer-1')     //give id of answer-1
+  answer1.style('font-size', '30px');
+  answer1.style('margin', '0px'); 
+
+  let Answer1 = document.getElementById('answer-1'); 
+  colorInput.appendChild(Answer1); //documenting its id, grabbing that element and giving it a variable
+  Answer1.innerHTML = answers; // a method where ur changing the words inside paragraph tag. Based on these conditional statements, the string it returns is different
+//innerhtml is just saying to change what the html is
+
+let picture = document.createElement('img');
+  colorInput.appendChild(picture);
+
+  
+  
+
+  if ((answers == "ramen") || (answers == "Ramen")) { //if what I've written... == is just checking that char is all the same, === evaluate data as well
+    picture.src = './images/ramen.png'; 
+    Answer1.innerHTML = answers; 
+    let a = createA('next.html', 'next');  
+    a.id('next'); //make id of next
+    colorInput.appendChild(next);
+  } else if ((answers == "curry") || (answers == "Curry")){
+    picture.src = './images/curry.png'; 
+  } else if ((answers == "sushi") || (answers == "Sushi")){
+    background(0,255,0);
+    picture.src = './images/sushi.png'; 
+  } else {
+    Answer1.innerHTML = "Please choose another food";
+  }
+}
+
